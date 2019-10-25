0e621eb0ebdeec15584db84598183c84fcac72cf
unknown <grantludwig97@gmail.com>
2019-10-25 11:46:23 -0700

    bad way removed, bubble sort added

--- ludwig_grant_AS10.py
+++ ludwig_grant_AS10.py
@@ -8,50 +8,45 @@
 # Sort 3 numbers
 
 points = 0
-
-def min(num1, i1, num2, i2):
-	global points
-	if num1 < num2:
-		points += 1
-		return i1, i2
-	else:
-		points += 1
-		return i2, i1
-
-def sort(list):
-	global points
-	firstIndex = -1
-	middleIndex = -1
-	lastIndex = -1
-	
-	# compare index 0 & 1 and 1 & 2
-	firstMinIndex, firstMaxIndex = min(list[0], 0, list[1], 1)
-	secondMinIndex, secondMaxIndex = min(list[1], 1, list[2], 2)
-	
-	if firstMinIndex == secondMinIndex:
-		points += 1
-		firstIndex = firstMinIndex
-		middleIndex, lastIndex = min(list[firstMaxIndex], firstMaxIndex, list[secondMaxIndex], secondMaxIndex)
-	else:
-		points += 1
-		firstIndex, middleIndex = min(list[firstMinIndex], firstMinIndex, list[secondMinIndex], secondMinIndex)
-		if firstMaxIndex != secondMinIndex and firstMaxIndex != secondMaxIndex:
-			points += 2
-			lastIndex = firstMaxIndex
-		else:
-			points += 2
-			lastIndex = secondMaxIndex
-		
-	return [list[firstIndex], list[middleIndex], list[lastIndex]]
+    
+def swapPositions(list, pos1, pos2):
+    list[pos1], list[pos2] = list[pos2], list[pos1]
+    return list
+    
+def bubbleSort(list):
+    global points
+    returnList = []
+    for num in list:
+        returnList.append(num)
+    
+    if returnList[0] > returnList[1]:
+        swapPositions(returnList, 0, 1)
+        points += 1
+    points +=1
+    if returnList[1] > returnList[2]:
+        swapPositions(returnList, 1, 2)
+        points += 1
+    points +=1
+    if returnList[0] > returnList[1]:
+        swapPositions(returnList, 0, 1)
+        points += 1
+    points +=1
+    
+    return returnList
 
 listOfList = ((1,2,3),(1,3,2),(2,1,3),(2,3,1),(3,1,2),(3,2,1))
+
+# bubble
+print("Bubble Sort")
+print("===========")
 for list in listOfList:
-	sorted = sort(list)
+	sorted = bubbleSort(list)
 	for num in sorted:
 		print(str(num))
 	print()
-	
+    
 print("Total Points: ", points)
+points = 0
 	
 #list = [1,2,3]
 #list = [1,3,2]
